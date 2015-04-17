9b14a9902a2a3af89c901761796b0b7d71a2b082
Dan McGough <mcgough.dan@gmail.com>
2015-04-17 01:10:40 -0700

    heroku deploy

--- public/js/script.js
+++ public/js/script.js
@@ -14,45 +14,6 @@ $(function(){
     })
   })
 
-  // $('.checkin-form').on('click',function(e){
-  //   e.preventDefault();
-  //   var form = $(this);
-  //   $.ajax({
-  //     method: form.attr('method'),
-  //     url: form.attr('action'),
-  //     data: form.serialize()
-  //   }).done(function(){
-  //     console.log('success');
-  //     // form.children('button').eq(1).removeClass('hidden');
-  //     $('.checkin-button').each(function(){
-  //       $(this).addClass('hidden');
-  //     });
-  //     $('.checkout-button').each(function(){
-  //       $(this).removeClass('hidden');
-  //     })
-  //     form.parent().parent().siblings().hide();
-  //   })
-  // })
-
-  // $('.checkout-form').on('click',function(e){
-  //   e.preventDefault();
-  //   var form = $(this);
-  //   $.ajax({
-  //     method: form.attr('method'),
-  //     url: form.attr('action'),
-  //     data: form.serialize()
-  //   }).done(function(){
-  //     console.log('deleted?');
-  //     $('.checkout-button').each(function(){
-  //       $(this).addClass('hidden');
-  //     })
-  //     $('.checkin-button').each(function(){
-  //       $(this).removeClass('hidden');
-  //     })
-  //     form.parent().parent().siblings().show();
-  //   })
-  // })
-
   $('.remove-form').on('click',function(e){
     e.preventDefault();
     var form = $(this);
@@ -74,7 +35,12 @@ $(function(){
       $(this).remove();
     }
   })
+
+  if($('.welcome') || $('.error') || $('.results')){
+    $('#header-search').focus();
+  }
 })
+
 
 var mapDiscreet = [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"on"},{"color":"#716464"},{"weight":"0.01"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.attraction","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"},{"color":"#a05519"},{"saturation":"-13"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#84afa3"},{"lightness":52}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"}]}]
 
