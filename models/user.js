1f63f7ab4601e50d109ba316276330ca392c98cb
Dan McGough <mcgough.dan@gmail.com>
2015-04-23 08:51:41 -0700

    possible auth fix?

--- models/user.js
+++ models/user.js
@@ -7,11 +7,8 @@ module.exports = function(sequelize, DataTypes) {
     email: {
       type: DataTypes.STRING,
       validate: {
-        isEmail: {
-          args: true,
-          msg: 'Please enter a valid email address'
+        isEmail: true
         }
-      }
     },
     username: {
       type: DataTypes.STRING,
