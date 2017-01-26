82d057ebc247f260140f34261f0af6916c009b7f
Dan <mcgough.dan@gmail.com>
2017-01-25 22:33:58 -0800

    geo added to favorites - frontend

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
+function distance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+          c(lat1 * p) * c(lat2 * p) * 
+          (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
 $(function(){
 
   $('#main-input').focus();
@@ -30,20 +40,18 @@ $(function(){
     })
   })
 
-//removes extra spaces in park list
-  $('.park-name').each(function(){
-    if($(this).text().length === 9){
-      $(this).remove();
-    }
-  })
-
-//adding heart to navbar on mobile
-
-  if($(window).innerWidth() <= 751) {
-    $('.header-heart').removeClass('hidden')
+  if (window.sessionStorage.coords) {
+    var coords = JSON.parse(window.sessionStorage.coords),
+        text = '';
+    $('.distance-placeholder').each(function() {
+      var dist = parseFloat(distance($(this).data('lat'),$(this).data('lng'),coords.lat,coords.lng)).toFixed(1);
+      text = dist + ' miles';
+      $(this).text(text);
+    })
   }
 
 })
+
 
 var mapDiscreet = [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"on"},{"color":"#716464"},{"weight":"0.01"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.attraction","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"},{"color":"#a05519"},{"saturation":"-13"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#84afa3"},{"lightness":52}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"}]}]
 
@@ -105,28 +113,6 @@ function parksMap(){
 }
 
 var openWindow = false;
-
-//Map on favorites/index.ejs
-function userMap(markers){
-  function initialize() {
-    var mapOptions = {
-      scrollwheel:false,
-      center: { lat: markers[0].lat, lng: markers[0].long},
-      zoom: 12,
-      styles: mapDiscreet
-      // disableDefaultUI: true
-    };
-    var map = new google.maps.Map(document.getElementById('user-map'), mapOptions);
-    markers.forEach(function(park){
-      console.log(park.lat, park.long);
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(park.lat,park.long)
-      });
-    marker.setMap(map);
-    })
-    google.maps.event.addDomListener(window, 'load', initialize);
-  }
-}
 
 //index.ejs script
 function indexJs(){
